98ce39418868d391223b953317881f5719765f44
Harsh Lamba <harshlamba70144@gmail.com>
2026-06-24 11:27:02 +0530

    implement board class to manage game state

---
+++ include/Core/Board.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Core/Bitboard.hpp"
+#include "Core/Move.hpp"
+
+namespace Engine {
+    enum Color { WHITE, BLACK, COLOR_COUNT };
+    enum Piece { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, PIECE_TYPE_COUNT };
+
+    class Board {
+    public:
+        U64 pieceBitboards[COLOR_COUNT][PIECE_TYPE_COUNT];
+        U64 occupancies[COLOR_COUNT];
+        U64 allPieces;
+
+        Board();
+        void initStandardBoard();
+        void updateOccupancies();
+        void printBoard();
+        bool getPieceAt(int square, int& outColor, int& outPiece) const;
+        void makeMove(Move move, int color);
+    };
+}
